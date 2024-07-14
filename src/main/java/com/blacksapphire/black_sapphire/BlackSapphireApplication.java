4df3b0dd06b1dab622d3e7b693775eea212ebccd
Lenard-Mate <57682558+Lenard-Mate@users.noreply.github.com>
2024-07-14 19:41:05 +0300

    Project creation.

---
+++ src/main/java/com/blacksapphire/black_sapphire/BlackSapphireApplication.java
@@ -0,0 +1,13 @@
+package com.blacksapphire.black_sapphire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlackSapphireApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlackSapphireApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/blacksapphire/black_sapphire/BlackSapphireApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blacksapphire.black_sapphire;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlackSapphireApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
